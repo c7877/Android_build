3ed23186dc90da016561ae3bba3595b3e07dbabc
q7954 <107266885+q7954@users.noreply.github.com>
2022-08-31 21:05:11 +0800

    Update diy-T.sh

--- diy-T.sh
+++ diy-T.sh
@@ -10,8 +10,7 @@ sed -i 's/TVBox/X影视/g' $CURRENT_DIR/$DIR/app/src/main/res/values-zh/strings.
 sed -i 's/TVBox/X影视/g' $CURRENT_DIR/$DIR/app/src/main/res/values/strings.xml
 #图标修改
 mv $CURRENT_DIR/DIY/app_icon.png $CURRENT_DIR/$DIR/app/src/main/res/drawable/app_icon.png
-#背景修改
-mv $CURRENT_DIR/DIY/app_bg.png $CURRENT_DIR/$DIR/app/src/main/res/drawable/app_bg.png
+
 #取消选集全屏
 sed -i 's/if (showPreview \&\& !fullWindows) toggleFullPreview/\/\/if (showPreview \&\& !fullWindows) toggleFullPreview/g' $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
 #缩略图清晰度修改
