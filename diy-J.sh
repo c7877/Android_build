8a8eb3f960340de4fb73f2bcaed951b5bfa975cd
c7877 <111748626+c7877@users.noreply.github.com>
2022-09-04 10:14:37 +0800

    Update diy-J.sh

--- diy-J.sh
+++ diy-J.sh
@@ -6,9 +6,8 @@ num=$(find $CURRENT_DIR -name gradlew  | awk -F"/" '{print NF-1}')
 DIR=$(find $CURRENT_DIR -name gradlew  | cut -d \/ -f$num)
 cd $CURRENT_DIR/$DIR
 #名称修改
-sed -i 's/TVBox/极影视/g' $CURRENT_DIR/$DIR/app/src/main/res/values/strings.xml
-#背景修改
-mv $CURRENT_DIR/DIY/app_bg.png $CURRENT_DIR/$DIR/app/src/main/res/drawable/app_bg.png
+sed -i 's/TVBox/X影视/g' $CURRENT_DIR/$DIR/app/src/main/res/values/strings.xml
+
 #取消选集全屏
 sed -i 's/if (showPreview \&\& !fullWindows) toggleFullPreview/\/\/if (showPreview \&\& !fullWindows) toggleFullPreview/g' $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
 #缩略图清晰度修改
